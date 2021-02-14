8d9c6d00504779476ab1ec61caf335f7a8481c54
Adam Jochemczyk <adamjoche@gmail.com>
2021-02-14 13:11:32 +0100

    Inputs to config pomodoro sessions

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,38 @@
-var minutes = 0;
-var seconds = 10;
+let minutes = 0;
+let seconds = 10;
 
-var click = new Audio("click.wav");
-var bell = new Audio("gong.wav");
+const click = new Audio("click.wav");
+const bell = new Audio("gong.wav");
 
 let minutes_interval:number;
 let seconds_interval:number;
 
+const clockMinutes=document.getElementById("minutes")
+const clockSeconds=document.getElementById("seconds")
+const done=document.getElementById("done")
+const playBtn=document.getElementById("play")
+const settingsBtn=document.getElementById("settings")
+const settingsForm=document.getElementsByClassName("settingsForm")
+
 const template=()=>{
-  document.getElementById("minutes").textContent = minutes.toString();
-  document.getElementById("seconds").textContent = seconds.toString();
+  clockMinutes.textContent = minutes.toString();
+  clockSeconds.textContent = seconds.toString();
 }
 
-  const minutesTimer=()=> {
-    minutes = minutes--;
-    document.getElementById("minutes").textContent = minutes.toString();
-  }
+const minutesTimer=()=> {
+  minutes--;
+  clockMinutes.textContent = minutes.toString();
+}
 
-  const secondsTimer=()=> {
-    seconds = seconds--;
-    document.getElementById("seconds").textContent = seconds.toString();
+const secondsTimer=()=> {
+    seconds--;
+    clockSeconds.textContent = seconds<10 ? `0${seconds}`: `${seconds}`;
     if (seconds <= 0) {
       if (minutes <= 0) {
         clearInterval(minutes_interval);
         clearInterval(seconds_interval);
-        document.getElementById("done").textContent =
-          "Session completed! Take a break";
-        document.getElementById("done").classList.add("show_message");
+        done.textContent ="Session completed! Take a break";
+        done.classList.add("show_message");
         bell.play()
       }
       seconds = 60;
@@ -35,15 +41,16 @@ const template=()=>{
 
 const start=()=> {
   click.play();
-  console.log("start")
+  minutes = 1;
+  seconds = 10;
+  clockMinutes.textContent = minutes.toString();
+  clockSeconds.textContent = seconds.toString();
 
-  minutes = 24;
-  seconds = 59;
-  document.getElementById("minutes").textContent = minutes.toString();
-  document.getElementById("seconds").textContent = seconds.toString();
-
-  minutes_interval = setInterval(minutesTimer, 60000);
-  seconds_interval = setInterval(secondsTimer, 1000);
+  minutes_interval = setInterval(()=>minutesTimer(), 60000);
+  seconds_interval = setInterval(()=>secondsTimer(), 1000);
 }
 
-document.getElementById("play").addEventListener("click",()=>start())
+playBtn.addEventListener("click",()=>start())
+settingsBtn.addEventListener("click",()=>{
+  settingsForm[0].classList.toggle("settingsForm--show")
+})
